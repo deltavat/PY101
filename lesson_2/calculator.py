dd0db309a4f061216512896e46f753acc3948d4f
Vatsal Patel <vatsalp@live.com>
2025-12-27 00:39:07 +0000

    Lesson 2 updates

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,28 +1,46 @@
-# Ask the user for the first number.
-# Ask the user for the second number. 
-# Ask the user for an operation to perform. 
-# Perform the operation on the two numbers.
-# Print the result to the terminal.
+def prompt(message):
+    print(f"==> {message}")
 
-print('Welcome to Calculatron 9000!')
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
 
-# Ask the user for the first number
-print("What's the first number?")
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
 number1 = input()
 
-print("What's the second number?")
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
 number2 = input()
 
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt("""What operation would you like to perform
+1) Add 2) Subtract 3) Multiply 4) Divide""")
 operation = input()
 
-if operation == '1':   						# add
-	output = int(number1) + int(number2)
-elif operation == '2': 						# subtract
-	output = int(number1) - int(number2)
-elif operation == '3': 						# multiply
-	output = int(number1) * int(number2)
-elif operation == '4': 						# divide
-	output = int(number1) / int(number2)
-	
-print(f"The result is: {output}")
+while operation not in ["1", "2", "3", "4"]:
+    prompt("You must choose 1, 2, 3, or 4")
+    operation = input()
+
+match operation:
+    case "1":
+        output = int(number1) + int(number2)
+    case "2":
+        output = int(number1) - int(number2)
+    case "3":
+        output = int(number1) * int(number2)
+    case "4":
+        output = int(number1) / int(number2)
+
+prompt(f"The result is {output}")
