e3ccaebd6daa1c5b34620b69e9fb1fe0f77e8b9c
Vatsal Patel <vatsalp@live.com>
2025-12-28 10:31:59 +0000

    Calculator updates

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,46 +1,58 @@
+import json
+
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
+def messages(message, lang='fr'):
+    return MESSAGES[lang][message]
+
 def prompt(message):
     print(f"==> {message}")
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
 
-prompt('Welcome to Calculator!')
+prompt(messages('welcome'))
 
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+while True:
+    prompt(messages('first'))
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt(messages('invalid_number'))
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt(messages('second'))
     number2 = input()
 
-prompt("""What operation would you like to perform
-1) Add 2) Subtract 3) Multiply 4) Divide""")
-operation = input()
+    while invalid_number(number2):
+        prompt(messages('invalid_number'))
+        number2 = input()
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+    prompt(messages('operation'))
     operation = input()
 
-match operation:
-    case "1":
-        output = int(number1) + int(number2)
-    case "2":
-        output = int(number1) - int(number2)
-    case "3":
-        output = int(number1) * int(number2)
-    case "4":
-        output = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt(messages('operation_choice'))
+        operation = input()
 
-prompt(f"The result is {output}")
+    match operation:
+        case "1":
+            output = float(number1) + float(number2)
+        case "2":
+            output = float(number1) - float(number2)
+        case "3":
+            output = float(number1) * float(number2)
+        case "4":
+            output = float(number1) / float(number2)
+
+    prompt(f"The result is {output}")
+    prompt(messages('repeat'))
+    answer = input()
+    if answer and answer.lower() != 'y':
+        break
